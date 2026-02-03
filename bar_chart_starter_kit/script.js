fe2eafb63573a341375d6ed3a0e7768aacdae2ff
Jerie <jerie.stolbergrohr@gmail.com>
2026-02-03 10:16:29 +0100

    array fungerer

--- bar_chart_starter_kit/script.js
+++ bar_chart_starter_kit/script.js
@@ -1,5 +1,21 @@
+
 const list = document.querySelector("ul");
 
 const li = document.createElement("li");
 li.style.setProperty("--height", "30");
 list.appendChild(li);
+
+const columnArray = [];
+
+setInterval(generateColumns, 1000);
+
+function generateColumns () {
+    columnArray.push(Math.round(Math.random()* 100));
+   console.log ("columnArray", columnArray);
+   if (columnArray.length > 20) {
+    columnArray.shift()
+   }
+}
+
+//length for længde
+//shift til start af array
