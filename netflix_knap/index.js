35316ab5ff3359467e6719750cea1ebb7b6da66f
Jerie <jerie.stolbergrohr@gmail.com>
2026-02-13 12:45:02 +0100

    lidt mere pynt

--- netflix_knap/index.js
+++ netflix_knap/index.js
@@ -1,6 +1,8 @@
 addEventListener("animationend", skiftBaggrundsfarve);
 addEventListener("mousemove", stopsbf);
 
+
+
 function skiftBaggrundsfarve () {
     document.querySelector("section").style.backgroundColor ="rgb(185, 95, 249)";
     if (stopsbf()) {
