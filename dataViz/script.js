cf1ca54bf09e6de3d8f0733548b407ee08f6bfd2
Jerie <jerie.stolbergrohr@gmail.com>
2026-02-13 10:21:30 +0100

    javadel virker

---
+++ dataViz/script.js
@@ -0,0 +1,14 @@
+const no = document.querySelector("#number");
+const artist = document.querySelector("#artist");
+update ();
+function update () {
+fetch ("https://kea-alt-del.dk/kata-distortion/")
+.then(response => response.json()) //response er selvvalgt navn her
+.then(data => {
+    console.log(data);
+
+    no.textContent =data.inQueue;
+    artist.textContent = data.name;
+})
+}
+setInterval(update, 10000)
